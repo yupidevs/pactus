1ae7c9aaf9ba5a2b5df2e4df89e9d154df15545a
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-10-07 14:26:10 -0400

    Add featurizer

---
+++ yuca/features/featurizer.py
@@ -0,0 +1,43 @@
+import numpy as np
+from pathlib import Path
+
+from yuca import config
+from yuca.dataset._utils import _get_path
+from yuca.dataset import Dataset, DatasetSlice
+from yuca.features.features import get_feat_vec
+
+FeatureMask = int
+
+class Featurizer:
+
+    def __init__(self, selected: FeatureMask, recompute: bool = False, **kwargs):
+        self.selected = selected
+        self.recompute = recompute
+        self.kwargs = kwargs
+    
+    def _recompute_required(self, feat_file: Path) -> bool:
+        return self.recompute or not feat_file.exists()
+
+    def _recompute(self, dataset: Dataset, feat_file: Path) -> np.ndarray:
+        feats = np.stack(
+                [
+                    get_feat_vec(traj, self.selected, **self.kwargs)
+                    for traj in dataset.trajs
+                ]
+            )
+        np.savetxt(str(feat_file), feats)
+        return feats
+
+    def compute(self, data: Dataset | DatasetSlice) -> np.ndarray:
+        """Computes the features matrix for a given dataset or slice."""
+
+        feats = None
+        dataset = data if isinstance(data, Dataset) else data.dataset
+        file_name = f"{self.selected}.txt"
+        feat_file = _get_path(config.DS_FEATS_DIR, dataset.name) / file_name
+        
+        # recompute if required
+        if self._recompute_required(feat_file):
+           feats = self._recompute() 
+        
+        return feats or np.loadtxt(feat_file)
