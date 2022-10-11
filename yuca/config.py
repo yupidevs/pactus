bd37d715f63036cd4bffa7ab8eefcdf90e3baca6
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-10-11 12:09:04 -0400

    Change cache path check using get

--- yuca/config.py
+++ yuca/config.py
@@ -34,11 +34,7 @@ PROGRESS_BAR_LENGTH = 50
 #          │   └── [serialized yupi trajectories]
 #          └── metadata.json
 #
-CACHE_PATH = (
-    str(Path(__file__).parent / ".cache")
-    if "YUCA_CACHE_PATH" not in os.environ
-    else os.environ["YUCA_CACHE_PATH"]
-)
+CACHE_PATH = os.environ.get("YUCA_CACHE_PATH", str(Path(__file__).parent / ".cache"))
 DS_BASE_DIR = CACHE_PATH + "/datasets"
 DS_DIR = DS_BASE_DIR + "/{0}"
 DS_METADATA_FILE = DS_DIR + "/metadata.json"
