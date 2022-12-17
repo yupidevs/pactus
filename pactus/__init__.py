b8de563371a92459ea4f0cc8c4fd2cf4c83842e5
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-12-17 15:35:59 +0000

    Set version v0.1.1a5

--- pactus/__init__.py
+++ pactus/__init__.py
@@ -6,7 +6,7 @@ from yupi.core import featurizers
 from pactus.dataset import Dataset
 from pactus.models import Evaluation, EvaluationComparison
 
-__version__ = "0.1.1a4"
+__version__ = "0.1.1a5"
 
 __all__ = [
     "Dataset",
